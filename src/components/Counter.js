cf23c6c97afcc0d0d7aebae85c0bb8d6f073df58
Zeehan <zeehanhub@gmail.com>
2020-04-20 13:10:59 +1000

    First Commit and created Counter component

---
+++ src/components/Counter.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Counter extends React.Component {
+    render () {
+        return (
+            <div>
+                <h1>Hello from Counter app</h1>
+            </div>
+        )
+    }
+}
+
+export default Counter;
