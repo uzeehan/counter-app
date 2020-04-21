4977296b0a0e39bc732c6398a6d07c0f8112cd2c
Zeehan <zeehanhub@gmail.com>
2020-04-21 10:01:07 +1000

    added increment and decrement functions

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,15 +1,38 @@
 import React, { Fragment } from 'react';
 
 class Counter extends React.Component {
+
+    constructor(probs) {
+        super(probs);
+
+        this.state = {
+            counter: 15
+        }
+    }
+
+    onClickPlusBtn = () => {
+        this.setState({
+            counter: this.state.counter + 1
+        })
+        console.log('clicked btn +');
+    }
+
+    onClickMinusBtn = () => {
+        this.setState({
+            counter: this.state.counter - 1
+        })
+        console.log('clicked btn -');
+    }
+
     render () {
         return (
             <div className="page-container">
                 <h1>Counter App</h1>
 
                 <ul>
-                    <li><button>-</button></li>
-                    <li>display counter</li>
-                    <li><button>+</button></li>
+                    <li><button name="minusBtn" onClick={this.onClickMinusBtn}>-</button></li>
+                    <li>{ this.state.counter }</li>
+                    <li><button name="plusBtn" onClick={this.onClickPlusBtn}>+</button></li>
                 </ul>
             </div>
         )
